a53ea3ae96c0c0c57273e8f9340eeef92b749926
Reece Carolan <reece@mycopilot.com>
2024-06-08 22:51:53 -0700

    feat: add link to Hello Recipes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,8 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
 import NextLink from 'next/link';
-import { Button } from '@mui/material';
+import { Button, Card, CardActionArea, Grid } from '@mui/material';
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -18,13 +19,42 @@ export default function Home() {
           alignItems: 'center',
         }}
       >
-        <Typography variant="h1" sx={{ mb: 2 }}>
+        <Typography variant="h1" sx={{ mb: 1 }}>
           Open Zero
         </Typography>
-        <Typography sx={{ mb: 2 }}>
-          Open source software. More to come :)
-        </Typography>
-        <Button href="https://github.com/open-zero">GitHub</Button>
+        <Typography sx={{ mb: 1 }}>Open source software</Typography>
+        <Button href="https://github.com/open-zero" sx={{ mb: 4 }}>
+          GitHub
+        </Button>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6} md={4}>
+            <Card variant="outlined">
+              <CardActionArea sx={{ p: 2 }}>
+                <Box sx={{ display: 'flex', justifyContent: 'center', mb: 2 }}>
+                  <Image
+                    src="/images/hello-recipes-logo.svg"
+                    alt="hello recipes"
+                    width={100}
+                    height={100}
+                  />
+                </Box>
+                <Typography variant="h2">Hello Recipes</Typography>
+                <Typography sx={{ mb: 2 }}>
+                  Save, savor, and share recipes. A modern recipe manager.
+                </Typography>
+                <Button
+                  href="https://hellorecipes.com"
+                  target="_blank"
+                  variant="contained"
+                  size="small"
+                  fullWidth
+                >
+                  Hello Recipes
+                </Button>
+              </CardActionArea>
+            </Card>
+          </Grid>
+        </Grid>
       </Box>
     </Container>
   );
